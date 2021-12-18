48a4b7940eb950b707f012132bbb89b28375765e
Alex <altsyp@gmail.com>
2021-12-18 13:57:02 +0400

    Added attachment support and default credentials, some refactoring.

--- email_sender.py
+++ email_sender.py
@@ -1,20 +1,44 @@
 #!/usr/bin/env python3
 
 from email.message import EmailMessage
+import os
+import mimetypes
 import smtplib
 import getpass
 
-import debug_print
+_MAIL_SERVER = "mail.stub.net"
+_MAIL_PWD = "qwertypass"
+_MAIL_I = "me"
 
-def send_email(from, to, subject, text, srvr_host, password):
-    ### Compiles email from given data and attemts to send it. If exception is raised (no mail server configured/available) attempts to start new one locally
+def send_byme(to, subject, text):
+    ### sends email with default sender, mail server and password
+    send_email(_MAIL_I, to, subject, text, _MAIL_SERVER, _MAIL_PWD)
+
+def send_byme_attach(to, subject, text, attach):
+    ### sends email w attachment with default sender, mail server and password
+    send_email_attached(_MAIL_I, to, subject, text, attach, _MAIL_SERVER, _MAIL_PWD)
+
+def send_email_attached(from, to, subject, text, attach, srvr_host, password):
+    ### Compiles email from given data, adds attachment and attemts to send it.
 
     # make message
-    msg = EmailMessage()
-    msg['From'] = from
-    msg['To'] = to
-    msg['Subject'] = Subject
-    msg.set_content(text)
+    msg = compile_email(from, to, subject, text)
+    #add attachment
+    msg = add_file(msg, attach)
+    # attempt to access mail server at srvr_host
+    actually_send(msg, srvr_host, password)
+
+
+def send_email(from, to, subject, text, srvr_host, password):
+    ### Compiles email from given data and attemts to send it.
+
+    # make message
+    msg = compile_email(from, to, subject, text)
+    # attempt to access mail server at srvr_host
+    actually_send(msg, srvr_host, password)
+
+def actually_send(email, srvr_host, password):
+    ### Attempts to send email via given credentials. Fails if no mail server configured/available
 
     # attempt to access mail server at srvr_host
     try:
@@ -28,4 +52,22 @@ def send_email(from, to, subject, text, srvr_host, password):
     #login & send
     pwd = getpass.getpass(password)
     s.login(from, pwd)
-    s.send_email(msg)
+    s.send_email(email)
+
+# construct email object
+# requires sender, reciever, subject and text
+def compile_email(from, to, subject, text):
+    msg = EmailMessage()
+    msg['From'] = from
+    msg['To'] = to
+    msg['Subject'] = Subject
+    msg.set_content(text)
+    return msg
+
+# guesses mime type of attachment and adds it to existing email
+def add_file(msg, attach):
+    m_type, _ = mimetypes.guess_type(attach)
+    m_type, m_subtype = mime_type.split('/', 1)
+    with open(attach, 'rb') as ap:
+        msg.add_attachment(ap.read(), maintype = m_type, subtype = m_subtype, filename = os.path.basename(attach))
+    return msg
